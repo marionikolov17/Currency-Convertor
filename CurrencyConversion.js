a83721600ba0f0367a4e566895ddd6c0804bd9cb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-06-08 20:11:02 +0300

    Abstract code for getting command line date

--- CurrencyConversion.js
+++ CurrencyConversion.js
@@ -2,32 +2,37 @@ const fs = require("fs").promises;
 
 const dateRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g;
 
-const getApiKey = async () => {
-    try {
-        const config = JSON.parse(await fs.readFile("./config.json", "utf-8"));
-
-        return config["API_KEY"];
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function main() {
+const getDate = () => {
   // Check if there is command line argument
   if (process.argv.length === 2) {
-    console.log("You must specify date!");
-    return;
+    throw new Error("You must specify date!");
   }
   let date = process.argv[2];
 
   // Check if command line argument is correct type
   if (!dateRegex.test(date)) {
-    console.log("You must specify valid date format - e.g '2024-05-12'!");
-    return;
+    throw new Error("You must specify valid date format - e.g '2024-05-12'!");
   }
+
+  return date;
+};
+
+const getApiKey = async () => {
+  try {
+    const config = JSON.parse(await fs.readFile("./config.json", "utf-8"));
+
+    return config["API_KEY"];
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+async function main() {
+  const date = getDate();
 
   const apiKey = await getApiKey();
   console.log(apiKey);
+  console.log(date);
 }
 
 main();
